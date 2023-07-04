db08e48a31ee0946865ac86835ff38284e7f6f94
brambooo <bram_reinold@hotmail.com>
2023-07-04 08:21:11 +0200

    PlatformIconList added

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,8 @@
-interface Game {
+import { Platform } from "./platform";
+
+export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: { platform: Platform }[]; // API design smell
 }
