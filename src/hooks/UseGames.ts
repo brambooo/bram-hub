5ae706ee117cb57974abef983e8d2133e2cb765c
brambooo <bram_reinold@hotmail.com>
2023-07-04 13:05:57 +0200

    skeleton added

--- src/hooks/UseGames.ts
+++ src/hooks/UseGames.ts
@@ -6,22 +6,28 @@ import { Game } from "../models/game";
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState([]);
+    const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
         const controller = new AbortController();
 
+        setLoading(true);
         apiClient.get<FetchGamesResponse>('/games')
-            .then(res => setGames(res.data.results))
+            .then(res => {
+                setLoading(false);
+                setGames(res.data.results);
+            })
             .catch(err => {
                 if (error instanceof CanceledError) return;
-                setError(err.message)
+                setError(err.message);
+                setLoading(false);
             }
             );
 
         return () => controller.abort();
     }, []);
 
-    return { games, error }
+    return { games, error, isLoading }
 }
 
 export default useGames;
